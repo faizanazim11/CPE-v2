7a1b7f40f3b5a0f32a193e36812f706004177329
Faizan Azim <faizanazim11@gmail.com>
2022-11-19 19:32:28 +0530

    module init

---
+++ platformservices/src/main/java/cpe/platformservices/PlatformservicesApplication.java
@@ -0,0 +1,13 @@
+package cpe.platformservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformservicesApplication.class, args);
+	}
+
+}
---
+++ platformservices/src/test/java/cpe/platformservices/PlatformservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.platformservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
